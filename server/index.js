9a141193183f084bc19536365eab68168edd110a
rtoshkov <rosentoshkov@gmail.com>
2022-08-09 15:35:04 +0300

    adding server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const DATABASE = `mongodb://127.0.0.1/arcane`;
+const cors = require('./middleware/cors.js');
+const attachToken = require('./middleware/attachToken.js');
+const figuresController = require('./controllers/figures.js');
+const usersController = require('./controllers/users.js');
+
+launch()
+
+async function launch() {
+    try {
+        await mongoose.connect(DATABASE, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log('Database connected');
+    } catch (err) {
+        console.error('Connection to Database failed');
+        process.exit(1);
+    }
+}
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(attachToken());
+
+app.use('/figures', figuresController);
+app.use('/users', usersController);
+
+app.get('/', (req, res) => res.json({message: 'ArcaneBG API'}))
+app.listen(3030, () => console.log('REST SERVICE running on port 3030'));
